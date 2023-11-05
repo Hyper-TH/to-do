b422325703b9ba0601365973942842550c13ff8d
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-05 22:59:36 +0000

    Initial commit for RepoInfo

---
+++ src/components/GitHub/RepoInfo.js
@@ -0,0 +1,23 @@
+export const UserInfo = (props) => {
+    return <>    
+      <div id='userRepos'>
+            <h1> User Repos </h1>
+            <div id="repoTableWrapper">
+                <table id='repoInfo'>
+                    <tbody id="Rlist">
+                        <tr>
+                            <th>Name: </th>
+                            <td>{props.name}</td>
+                        </tr>
+                        <tr>
+                            <th>Description: </th>
+                            <td>{props.description}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </>
+};
+
+
