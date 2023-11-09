62b4aaf1a7fb068e7c3f4a42d9564b096cd7f118
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-09 10:34:38 +0000

    Modified to be able to get server message

--- src/pages/ServerTest.js
+++ src/pages/ServerTest.js
@@ -1,7 +1,19 @@
-export const ServerTest = () =>{
+import { useState, useEffect } from 'react';
+
+export const ServerTest = () => {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch('http://localhost:8000/message')
+        .then((res) => res.json())
+        .then((data) => setMessage(data.message));
+    }, []);
+
     return (
         <>
-            <div>Hello there</div>
+        <div>
+            <h1>{message}</h1>  
+        </div>
         </>
     );
-}
+};
