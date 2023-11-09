e3fe1f75b7fbb6254b894198e759a30f14b273f9
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-09 11:24:44 +0000

    Successful pull request using axios

--- src/pages/ServerTest.js
+++ src/pages/ServerTest.js
@@ -1,18 +1,28 @@
+import Axios from 'axios';
 import { useState, useEffect } from 'react';
 
 export const ServerTest = () => {
     const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch('http://localhost:8000/message')
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
-    }, []);
+    // Using in-built library
+    // useEffect(() => {
+    //     fetch('http://localhost:8000/message')
+    //     .then((res) => res.json())
+    //     .then((data) => setMessage(data.message));
+    // }, []);
 
+    // Using axios
+    useEffect(() => {
+        Axios.get(`http://localhost:8000/message`)
+        .then((res) => {
+            setMessage(res.data);
+        })
+    }, []);
+    
     return (
         <>
         <div>
-            <h1>{message}</h1>  
+            <h1>{message?.message}</h1>  
         </div>
         </>
     );
