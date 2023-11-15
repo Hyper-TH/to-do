f673421c04ec2aa6bb1cdd78ec83e51ea658e79a
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-15 14:19:10 +0000

    Changed to ES6 import structure

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const exp = require('constants');
+import express from 'express';
+import cors from 'cors';
 
 // Create Express application
-const app =  express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Create a GET route
 // /message is an endpoint that returns JSON object with message
 app.get('/message', (req, res) => {
-    res.json({message: "Hello from server!"});
-})
+    res.json({ message: 'Hello from server!' });
+});
 
+// Get results from 'panadol' query on medicines.ie
 
 // Start server
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
-})
+});
