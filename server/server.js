72e1e408952388ae4978159ceef68cfae567dbdd
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-19 15:42:48 +0000

    Generate an excuse using the API through the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import Axios from 'axios';
 
 // Create Express application
 const app = express();
@@ -12,7 +13,26 @@ app.get('/message', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
 
-// Get results from 'panadol' query on medicines.ie
+// endpoint to generate an excuse
+app.get('/excuse', async (req, res) => {
+    try {
+        const { category } = req.query;
+    
+        if (!category) {
+            return res.status(400).json({ error: 'Category is required '});
+        }
+    
+        const apiURL = `https://excuser-three.vercel.app/v1/excuse/${category}`;
+        const response = await Axios.get(apiURL);
+
+        console.log(response.data);  // Display the response data
+        res.json(response.data);     // Send the response data to the client
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 // Start server
 app.listen(8000, () => {
--- src/pages/ServerTest.js
+++ src/pages/ServerTest.js
@@ -3,13 +3,32 @@ import { useState, useEffect } from 'react';
 
 export const ServerTest = () => {
     const [message, setMessage] = useState("");
+    const [category, setCategory] = useState("");
+    const [excuse, setExcuse] = useState("");
+    const [error, setError] = useState("");
 
-    // Using in-built library
-    // useEffect(() => {
-    //     fetch('http://localhost:8000/message')
-    //     .then((res) => res.json())
-    //     .then((data) => setMessage(data.message));
-    // }, []);
+    const categoryChange = (event) => {
+        setCategory(event.target.value);
+    };
+
+    const generateExcuse = async () => {
+        try {
+            const response = await Axios.get(`http://localhost:8000/excuse?category=${encodeURIComponent(category)}`);
+    
+            console.log(response);
+            if (response) {
+                setExcuse(response.data[0].excuse);
+                setError("");
+            } else {
+                setExcuse("");
+                setError("Error retrieving excuse");
+            }
+        } catch (error) {
+            console.error(`Error: ${error}`);
+            setExcuse("");
+            setError("Internal Server Error");
+        }
+    };
 
     // Using axios
     useEffect(() => {
@@ -23,6 +42,17 @@ export const ServerTest = () => {
         <>
         <div>
             <h1>{message?.message}</h1>  
+        </div>
+
+        <div>
+            <label>
+                Input a category:
+                <input type="text" value={category} onChange={categoryChange} />
+            </label>
+            <button onClick={generateExcuse}>Generate an excuse!</button>
+
+            {excuse && <p>Ecxuse: {excuse}</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
         </>
     );
