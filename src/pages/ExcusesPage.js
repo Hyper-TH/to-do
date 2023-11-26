e4b52eea348e49eea6fd03703b142c04f699daf4
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-26 05:42:14 +0000

    Successfully display data from firestore

--- src/pages/ExcusesPage.js
+++ src/pages/ExcusesPage.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 export const ExcusePage = () => {
+    const [excuses, setExcuses] = useState([]);
+    const [error, setError] = useState("");
+    
+    useEffect(() => {
+        const fetchExcuses = async () => {
+            try {
+                // Fetch excuses from the server
+                const response = await Axios.get('http://localhost:8000/getExcuses');
+
+                if (response.data && response.data.documents) {
+                    setExcuses(response.data.documents);
+                    setError("");
+                } else {
+                    setError("Error retrieving excuses");
+                }
+            } catch (error) {
+                console.error(`Error: ${error}`);
+                setError("Internal Server Error");
+            }
+        };
+
+        // Call function to fetch once component mounts
+        fetchExcuses();
+    }, []);
+
     return (
         <>
-            This is the excuses page
+        <div>
+            <h1>Excuses Page</h1>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+            {excuses.map((excuse, index) => (
+                <div key={index}>
+                    <p>Category: {excuse.category}</p>
+                    <p>Excuse: {excuse.excuse}</p>
+                </div>
+            ))}
+        </div>
         </>
     )
 };  
